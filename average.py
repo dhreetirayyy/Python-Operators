e66ba4dd1af9651a8d44560a7d0a370ae6b0255b
dhreetirayyy <dkumar.deo@gmail.com>
2025-03-06 16:47:36 -0800

    Printing Average and Sum

---
+++ average.py
@@ -0,0 +1,9 @@
+tree1 = 57
+tree2 = 94
+tree3 = 69
+tree4 = 81
+tree5 = 41
+sum = tree1+tree2+tree3+tree4+tree5
+print("Sum of all 5 trees is:" , sum)
+average = sum/5
+print("Average of all 5 trees:" , average)
